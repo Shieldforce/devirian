147a69cf325a2a64b50333d6b9272c958d4bdcc2
Alxandre Ferreira <shieldforce2@gmail.com>
2023-04-01 21:33:50 -0300

    chat

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: ["vuetify"],
   devServer: {
     allowedHosts: "all",
     client: {
       webSocketURL: "ws://35.215.227.4:9999",
     },
   },
+  transpileDependencies: ["vuetify"],
 });
